88896641d287aed95828b6b8e32877e3cbb1c144
Forge Agent <forge-agent@users.noreply.github.com>
2026-07-31 16:58:45 +0000

    Refactor TaskDate component to eliminate button duplication

--- src/components/TaskDate.js
+++ src/components/TaskDate.js
@@ -3,79 +3,91 @@ import moment from 'moment';
 import { FaSpaceShuttle, FaSun, FaRegPaperPlane } from 'react-icons/fa';
 import PropTypes from 'prop-types';
 
+const DATE_BUTTONS = [
+  {
+    id: 'today',
+    label: 'Today',
+    icon: FaSpaceShuttle,
+    testId: 'task-date-today',
+    ariaLabel: 'Select today as the task date',
+    getDate: () => moment().format('DD/MM/YYYY'),
+  },
+  {
+    id: 'tomorrow',
+    label: 'Tomorrow',
+    icon: FaSun,
+    testId: 'task-date-tomorrow',
+    ariaLabel: 'Select tomorrow as the task date',
+    getDate: () => moment().add(1, 'day').format('DD/MM/YYYY'),
+  },
+  {
+    id: 'next-week',
+    label: 'Next week',
+    icon: FaRegPaperPlane,
+    testId: 'task-date-next-week',
+    ariaLabel: 'Select next week as the task date',
+    getDate: () => moment().add(7, 'days').format('DD/MM/YYYY'),
+  },
+];
+
+const DateButton = ({ button, setTaskDate, setShowTaskDate }) => {
+  const Icon = button.icon;
+
+  const handleSelect = () => {
+    setShowTaskDate(false);
+    setTaskDate(button.getDate());
+  };
+
+  const handleKeyDown = (e) => {
+    if (e.key === 'Enter') {
+      handleSelect();
+    }
+  };
+
+  return (
+    <li>
+      <div
+        onClick={handleSelect}
+        onKeyDown={handleKeyDown}
+        data-testid={button.testId}
+        tabIndex={0}
+        aria-label={button.ariaLabel}
+        role="button"
+      >
+        <span>
+          <Icon />
+        </span>
+        <span>{button.label}</span>
+      </div>
+    </li>
+  );
+};
+
+DateButton.propTypes = {
+  button: PropTypes.shape({
+    id: PropTypes.string.isRequired,
+    label: PropTypes.string.isRequired,
+    icon: PropTypes.elementType.isRequired,
+    testId: PropTypes.string.isRequired,
+    ariaLabel: PropTypes.string.isRequired,
+    getDate: PropTypes.func.isRequired,
+  }).isRequired,
+  setTaskDate: PropTypes.func.isRequired,
+  setShowTaskDate: PropTypes.func.isRequired,
+};
+
 export const TaskDate = ({ setTaskDate, showTaskDate, setShowTaskDate }) =>
   showTaskDate && (
     <div className="task-date" data-testid="task-date-overlay">
       <ul className="task-date__list">
-        <li>
-          <div
-            onClick={() => {
-              setShowTaskDate(false);
-              setTaskDate(moment().format('DD/MM/YYYY'));
-            }}
-            onKeyDown={(e) => {
-              if (e.key === 'Enter') {
-                setShowTaskDate(false);
-                setTaskDate(moment().format('DD/MM/YYYY'));
-              }
-            }}
-            data-testid="task-date-today"
-            tabIndex={0}
-            aria-label="Select today as the task date"
-            role="button"
-          >
-            <span>
-              <FaSpaceShuttle />
-            </span>
-            <span>Today</span>
-          </div>
-        </li>
-        <li>
-          <div
-            onClick={() => {
-              setShowTaskDate(false);
-              setTaskDate(moment().add(1, 'day').format('DD/MM/YYYY'));
-            }}
-            onKeyDown={(e) => {
-              if (e.key === 'Enter') {
-                setShowTaskDate(false);
-                setTaskDate(moment().add(1, 'day').format('DD/MM/YYYY'));
-              }
-            }}
-            data-testid="task-date-tomorrow"
-            role="button"
-            tabIndex={0}
-            aria-label="Select tomorrow as the task date"
-          >
-            <span>
-              <FaSun />
-            </span>
-            <span>Tomorrow</span>
-          </div>
-        </li>
-        <li>
-          <div
-            onClick={() => {
-              setShowTaskDate(false);
-              setTaskDate(moment().add(7, 'days').format('DD/MM/YYYY'));
-            }}
-            onKeyDown={(e) => {
-              if (e.key === 'Enter') {
-                setShowTaskDate(false);
-                setTaskDate(moment().add(7, 'days').format('DD/MM/YYYY'));
-              }
-            }}
-            data-testid="task-date-next-week"
-            aria-label="Select next week as the task date"
-            tabIndex={0}
-            role="button"
-          >
-            <span>
-              <FaRegPaperPlane />
-            </span>
-            <span>Next week</span>
-          </div>
-        </li>
+        {DATE_BUTTONS.map((button) => (
+          <DateButton
+            key={button.id}
+            button={button}
+            setTaskDate={setTaskDate}
+            setShowTaskDate={setShowTaskDate}
+          />
+        ))}
       </ul>
     </div>
   );
